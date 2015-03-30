ee65eb0b3028a77c6020872e6ab1aa7f0a53a960
Jacek Możejko <jacek.mozejko@netguru.pl>
2015-03-30 18:17:16 +0200

    Update user profile feature specs
    [#91167810]

--- spec/features/user_profile_spec.rb
+++ spec/features/user_profile_spec.rb
@@ -1,19 +1,51 @@
 require 'spec_helper'
 
 describe 'profile', js: true do
-  let(:user) { create(:plain_user) }
+  let!(:junior) { create(:junior_role, priority: 3) }
+  let!(:developer) { create(:developer_role, priority: 2) }
+  let(:position) { create(:position, role_id: junior.id) }
 
   before do
-    page.set_rack_session 'warden.user.user.key' => User.serialize_into_session(user).unshift('User')
+    page.set_rack_session 'warden.user.user.key' => User
+      .serialize_into_session(position.user).unshift('User')
   end
 
   describe 'setting primary role' do
-    xit 'set primary role to a user' do
+    before { visit user_path(position.user.id) }
+
+    it 'set primary role to a user' do
+      expect(page).to have_select('js-user-primary', selected: 'no role')
+      select(position.role.name, from: 'js-user-primary')
+
+      within('form.edit_user') do
+        first('input[type=submit]').click
+      end
+
+      expect(page).to have_select('js-user-primary', selected: position.role.name)
     end
   end
 
   describe 'adding positions' do
-    xit 'adds a position to user' do
+    before { visit user_path(position.user.id) }
+
+    it 'adds a position to user' do
+      expect(page).to have_select('js-user-primary', options: ['no role', 'junior'])
+
+      within('form.edit_user') do
+        click_link('Add position')
+      end
+
+      within('form.new_position') do
+        select(developer.name, from: 'position_role_id')
+        select("#{position.user.last_name} #{position.user.first_name}",
+          from: 'position_user_id')
+
+        fill_in('position_starts_at', with: (position.starts_at + 1.year).strftime)
+        click_button('Create Position')
+      end
+
+      expect(page).to have_select('js-user-primary',
+        options: ['no role', 'junior', 'developer'])
     end
   end
 end
