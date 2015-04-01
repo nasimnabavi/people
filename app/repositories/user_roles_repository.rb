5639bf73c32e31952dea485607de704a04b27e23
Jacek Możejko <jacek.mozejko@netguru.pl>
2015-04-01 10:48:48 +0200

    Update user roles repository query
    [#90921698]

--- app/repositories/user_roles_repository.rb
+++ app/repositories/user_roles_repository.rb
@@ -6,7 +6,6 @@ class UserRolesRepository
   end
 
   def all
-    # Optimize for HMT relation once PostgreSQL is ready
-    @all ||= user.positions.includes(:role).map(&:role)
+    @all ||= user.roles
   end
 end
