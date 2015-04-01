c6b633bfa229aae4c090f18eeb399b2772928bd3
Błażej Pankowiak <blazej.pankowiak@netguru.pl>
2015-04-01 11:48:36 +0200

    Fix specs [#90918316]

--- spec/services/trello/card_sync_spec.rb
+++ spec/services/trello/card_sync_spec.rb
@@ -1,6 +1,11 @@
 require 'spec_helper'
 
 describe Trello::CardSync do
+  before do
+    allow_any_instance_of(UserRepository).to receive(:find_by_full_name)
+    allow_any_instance_of(ProjectsRepository).to receive(:find_or_create_by_name)
+  end
+
   context 'card has a label' do
     it 'calls AddUserToProject' do
       Trello::AddUserToProject.any_instance.should_receive :call!
