8e2218e86bc592fdcc17b8e1fb5c144c786025fa
Adam Nowak <lubieniebieski@gmail.com>
2015-04-07 17:39:04 +0200

    available users are greedy, very greedy

--- app/repositories/available_users_repository.rb
+++ app/repositories/available_users_repository.rb
@@ -1,6 +1,7 @@
 class AvailableUsersRepository
   def all
-    User.includes(:roles, :primary_role, :memberships, :positions, positions: [:role])
+    User.includes(:roles, :primary_role, :abilities)
+      .includes(positions: [:role], memberships: [:project])
       .includes(:current_memberships, :potential_memberships, :next_memberships, :booked_memberships, :last_membership)
       .where(available: true)
       .where(archived: false)
