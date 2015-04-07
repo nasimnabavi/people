033b8e6a36c6f587bcc69d24edf036631789425e
Błażej Pankowiak <blazej.pankowiak@netguru.pl>
2015-04-07 09:46:19 +0200

    Reuse card double in examples [a200bca87d3933f79afb962c890e020ec28a6a30]

--- spec/services/trello/card_sync_spec.rb
+++ spec/services/trello/card_sync_spec.rb
@@ -1,9 +1,14 @@
 require 'spec_helper'
 
 describe Trello::CardSync do
+  let(:card) { double('card') }
+
   before do
     allow_any_instance_of(UserRepository).to receive(:find_by_full_name)
     allow_any_instance_of(ProjectsRepository).to receive(:find_or_create_by_name)
+
+    card.stub(:name) { 'User Name' }
+    card.stub(:card_labels) { [{ 'name' => 'label' }] }
   end
 
   context 'card has a label' do
@@ -13,22 +18,14 @@ describe Trello::CardSync do
         .and_return(instance)
       instance.should_receive(:call!)
 
-      card = double('card')
-      card.stub(:name) { 'User Name' }
-      card.stub(:card_labels) { [{ 'name' => 'label' }] }
-
       described_class.new(card).call!
     end
   end
 
   context 'card does not have a label' do
     it 'calls RemoveUserFromProjects' do
-      expect_any_instance_of(Trello::RemoveUserFromProjects).to receive :call!
-
-      card = double('card')
-      card.stub(:name)
       card.stub(:card_labels) { [] }
-
+      expect_any_instance_of(Trello::RemoveUserFromProjects).to receive :call!
       described_class.new(card).call!
     end
   end
