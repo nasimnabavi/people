fcb8be23fa49ee037b3e66f8d6ecd280aa308e13
Błażej Pankowiak <blazej.pankowiak@netguru.pl>
2015-04-07 13:03:32 +0200

    Test proper user [cfb983c4efc9eb765c1960513d2d530530bf6379]

--- spec/services/trello/add_user_to_project_spec.rb
+++ spec/services/trello/add_user_to_project_spec.rb
@@ -35,7 +35,10 @@ describe Trello::AddUserToProject do
 
     it 'does not create a new membership for the user' do
       expect do
-        subject.call!
+        described_class.new(
+          "#{user_with_membership.first_name} #{user_with_membership.last_name}",
+          project.name
+        )
       end.to_not change{ user_with_membership.memberships.count }.by 1
     end
   end
