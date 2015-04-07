83d770a417e231301b5d73d360833ab38f43e6ae
Błażej Pankowiak <blazej.pankowiak@netguru.pl>
2015-04-07 10:35:05 +0200

    Test not included memberships separately [2b17067ce67c9da71666dd19ace55743792c75f7]

--- spec/repositories/membership_repository_spec.rb
+++ spec/repositories/membership_repository_spec.rb
@@ -1,17 +1,26 @@
 require 'spec_helper'
 
 describe MembershipsRepository do
-  describe ".upcoming_changes" do
+  describe '.upcoming_changes' do
     let(:days) { 30 }
+    let!(:membership1) { create(:membership, starts_at: 1.week.from_now) }
+    let!(:membership2) { create(:membership, starts_at: 1.week.from_now) }
+    let!(:future_membership) do
+      create(
+        :membership,
+        starts_at: 1.year.from_now,
+        ends_at: 2.years.from_now
+      )
+    end
+
     subject { described_class.new.upcoming_changes(days).to_a.flatten }
 
-    it "includes 2 upcoming changes" do
-      membership1 = create :membership, starts_at: 1.week.from_now
-      membership2 = create :membership, starts_at: 1.week.from_now
-      membership_not_included = create :membership, starts_at: 1.year.from_now,
-                                                    ends_at: 2.years.from_now
+    it 'includes 2 upcoming changes' do
       expect(subject).to include membership1, membership2
-      expect(subject).not_to include membership_not_included
+    end
+
+    it 'does not include other memberships' do
+      expect(subject).not_to include future_membership
     end
   end
 end
