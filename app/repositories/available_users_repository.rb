a620a0203c396a4a92c3338fb2b67162f98c9e9f
Jacek Możejko <jacek.mozejko@netguru.pl>
2015-04-15 13:17:53 +0200

    Fix available users primary_role N + 1 issue
    [#92132876]

--- app/repositories/available_users_repository.rb
+++ app/repositories/available_users_repository.rb
@@ -1,11 +1,11 @@
 class AvailableUsersRepository
   def all
-    User.includes(:roles, :primary_role, :abilities)
-      .includes(positions: [:role], memberships: [:project])
-      .includes(:current_memberships, :potential_memberships, :next_memberships, :booked_memberships, :last_membership)
-      .where(available: true)
-      .where(archived: false)
-      .where(primary_role: technical_roles_ids)
+    User
+      .includes(:roles, :abilities, :current_memberships,
+        :potential_memberships, :next_memberships, :booked_memberships,
+        :last_membership, positions: [:role], memberships: [:project],
+        primary_role: [:users])
+      .where(available: true, archived: false, primary_role: technical_roles_ids)
   end
 
   private
