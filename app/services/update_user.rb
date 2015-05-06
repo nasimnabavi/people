e387d9bb862696dba1ab2acd86a703521f919778
Jacek Możejko <jacek.mozejko@netguru.pl>
2015-05-06 17:07:03 +0200

    Minor syntax upgrades
    [#91378288]

--- app/services/update_user.rb
+++ app/services/update_user.rb
@@ -9,22 +9,20 @@ class UpdateUser
   def call
     create_new_abilities
     update_leader_commitment
-    user.attributes = params
-    user.save
+    user.update(params)
   end
 
   private
 
   def create_new_abilities
     # TODO: decouple this method from Selectize implementation
-    if params['ability_ids']
-      params['ability_ids'].shift
-      new_abilities = params['ability_ids'].select{ |a| a.match /\D/ }
-      new_abilities.each do |name|
-        ability = Ability.find_or_create_by name: name
-        params['ability_ids'].pop
-        params['ability_ids'] << ability.id.to_s
-      end
+    return unless params['ability_ids']
+    params['ability_ids'].shift
+    new_abilities = params['ability_ids'].select{ |a| a.match /\D/ }
+    new_abilities.each do |name|
+      ability = Ability.find_or_create_by name: name
+      params['ability_ids'].pop
+      params['ability_ids'] << ability.id.to_s
     end
   end
 
