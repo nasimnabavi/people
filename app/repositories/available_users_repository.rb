4795f9c5724021b1c1b4263f6d7e8f2c2c812a7c
Marcin Dobrowolski <marcin.dobrowolski@netguru.co>
2015-07-31 11:25:14 +0200

    AvailableUsersRepository#to_rotate
    [#99863356]

--- app/repositories/available_users_repository.rb
+++ app/repositories/available_users_repository.rb
@@ -8,6 +8,16 @@ class AvailableUsersRepository
     users_with_includes(User.where(primary_role: non_billable_technical_roles))
   end
 
+  def to_rotate
+    users_with_includes(
+      User.joins(memberships: [:project]).active.available.where(
+        primary_role: billable_technical_roles,
+        projects: { end_at: nil, internal: false },
+        memberships: { ends_at: nil }
+        ).merge(Project.active.nonpotential).order('memberships.starts_at ASC')
+      )
+  end
+
   private
 
   def users_with_includes(users)
@@ -15,17 +25,21 @@ class AvailableUsersRepository
       :roles,
       :abilities,
       :projects,
+      :longest_current_membership,
       current_memberships: [:project],
       potential_memberships: [:project],
       next_memberships: [:project],
       booked_memberships: [:project],
-      last_membership: [:project],
       positions: [:role],
       primary_role: [:users])
   end
 
   def non_billable_technical_roles
     @non_billable_technical_roles ||= Role.technical.non_billable
+  end
+
+  def billable_technical_roles
+    @billable_technical_roles ||= Role.technical.billable
   end
 
   def technical_roles_ids
