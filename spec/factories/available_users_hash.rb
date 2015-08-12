83f53d069883583834507613c404844005696fb4
Marcin Dobrowolski <marcin.dobrowolski@netguru.co>
2015-08-12 12:07:49 +0200

    [b59ab44c20ff1cad0528d298cc85c665617c039c]
    [#100288076]

--- spec/factories/available_users_hash.rb
+++ spec/factories/available_users_hash.rb
@@ -1,57 +1,61 @@
 FactoryGirl.define do
   factory :available_users_hash, class: Hash do
     project
-    dev_without_due_date { {
-      with_nothing_scheduled: create(:developer_in_project, project: project),
-      with_commercial_project_scheduled:
-        create(:developer_in_project, :with_project_scheduled,
-          project: project,
-          scheduled_project: create(:project, :without_due_date, :commercial),
-          scheduled_membership_start: 5.days.from_now
-        ),
-      with_internal_project_scheduled:
-        create(:developer_in_project, :with_project_scheduled,
-          project: project,
-          scheduled_project: create(:project, :without_due_date, :internal),
-          scheduled_membership_start: 5.days.from_now
-        ),
-      booked:
-        create(:developer_in_project, :with_project_scheduled,
-          project: project,
-          scheduled_project: create(:project, :without_due_date),
-          scheduled_membership_start: 5.days.from_now,
-          booked: true
-        )
-    } }
+    dev_without_due_date do
+      {
+        with_nothing_scheduled: create(:developer_in_project, project: project),
+        with_commercial_project_scheduled:
+          create(:developer_in_project, :with_project_scheduled,
+            project: project,
+            scheduled_project: create(:project, :without_due_date, :commercial),
+            scheduled_membership_start: 5.days.from_now
+          ),
+        with_internal_project_scheduled:
+          create(:developer_in_project, :with_project_scheduled,
+            project: project,
+            scheduled_project: create(:project, :without_due_date, :internal),
+            scheduled_membership_start: 5.days.from_now
+          ),
+        booked:
+          create(:developer_in_project, :with_project_scheduled,
+            project: project,
+            scheduled_project: create(:project, :without_due_date),
+            scheduled_membership_start: 5.days.from_now,
+            booked: true
+          )
+      }
+    end
 
-    dev_with_due_date { {
-      with_nothing_scheduled: create(:developer_in_project,
-        project: project,
-        membership_due_date: 5.days.from_now
-      ),
-      with_commercial_project_scheduled:
-        create(:developer_in_project, :with_project_scheduled,
+    dev_with_due_date do
+      {
+        with_nothing_scheduled: create(:developer_in_project,
           project: project,
-          scheduled_project: create(:project, :without_due_date, :commercial),
-          scheduled_membership_start: 5.days.from_now,
           membership_due_date: 5.days.from_now
         ),
-      with_internal_project_scheduled:
-        create(:developer_in_project, :with_project_scheduled,
-          project: project,
-          scheduled_project: create(:project, :without_due_date, :internal),
-          scheduled_membership_start: 5.days.from_now,
-          membership_due_date: 5.days.from_now
-        ),
-      booked:
-        create(:developer_in_project, :with_project_scheduled,
-          project: project,
-          scheduled_project: create(:project, :without_due_date),
-          scheduled_membership_start: 5.days.from_now,
-          membership_due_date: 5.days.from_now,
-          booked: true
-        )
-    } }
+        with_commercial_project_scheduled:
+          create(:developer_in_project, :with_project_scheduled,
+            project: project,
+            scheduled_project: create(:project, :without_due_date, :commercial),
+            scheduled_membership_start: 5.days.from_now,
+            membership_due_date: 5.days.from_now
+          ),
+        with_internal_project_scheduled:
+          create(:developer_in_project, :with_project_scheduled,
+            project: project,
+            scheduled_project: create(:project, :without_due_date, :internal),
+            scheduled_membership_start: 5.days.from_now,
+            membership_due_date: 5.days.from_now
+          ),
+        booked:
+          create(:developer_in_project, :with_project_scheduled,
+            project: project,
+            scheduled_project: create(:project, :without_due_date),
+            scheduled_membership_start: 5.days.from_now,
+            membership_due_date: 5.days.from_now,
+            booked: true
+          )
+      }
+    end
 
     initialize_with { attributes }
   end
