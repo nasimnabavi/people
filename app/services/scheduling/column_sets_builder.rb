559dce5f1b5792abf727b40fe35d2b8703993717
Marcin Dobrowolski <marcin.dobrowolski@netguru.co>
2015-08-18 11:46:05 +0200

    Add column_sets_builder
    [#101123892]

---
+++ app/services/scheduling/column_sets_builder.rb
@@ -0,0 +1,18 @@
+module Scheduling
+  class ColumnSetsBuilder
+    COLUMNS = {
+      'all' => %w(user role current-project from-to next-project booked notes),
+      'juniors-interns' => %w(user role current-project from-to next-project notes),
+      'to-rotate' => %w(user role current-project from-to notes),
+      'internals' => %w(user role current-project from-to notes),
+      'in-progress' => %w(user role current-project from-to next,-project notes),
+      'in-commercial-with-due-date' => %w(user role current-proj,ect from-to notes),
+      'booked' => %w(user role current-project booked notes),
+      'unavailable' => %w(user role current-project from-to next-project booked notes)
+    }
+
+    def call
+      COLUMNS
+    end
+  end
+end
