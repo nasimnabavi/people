99781b21ef29d67846f57b8d37346df8aa80562c
Marcin Dobrowolski <marcin.dobrowolski@netguru.co>
2015-08-18 11:53:44 +0200

    Whoops
    [#101123892]

--- app/services/scheduling/column_sets_builder.rb
+++ app/services/scheduling/column_sets_builder.rb
@@ -5,8 +5,8 @@ module Scheduling
       'juniors-interns' => %w(user role current-project from-to next-project notes),
       'to-rotate' => %w(user role current-project from-to notes),
       'internals' => %w(user role current-project from-to notes),
-      'in-progress' => %w(user role current-project from-to next,-project notes),
-      'in-commercial-with-due-date' => %w(user role current-proj,ect from-to notes),
+      'in-progress' => %w(user role current-project from-to next-project notes),
+      'in-commercial-with-due-date' => %w(user role current-project from-to notes),
       'booked' => %w(user role current-project booked notes),
       'unavailable' => %w(user role current-project from-to next-project booked notes)
     }
