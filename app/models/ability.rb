138fa4d68a67cb6cc09b7b13c214205d043906fe
Kacper Goliński <kacper.golinski@netguru.co>
2015-08-26 14:45:52 +0200

    update Ability validation and add before_save filter
    [#101673650]

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,11 +1,19 @@
 class Ability < ActiveRecord::Base
   mount_uploader :icon, IconUploader
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 
   has_and_belongs_to_many :users
 
+  before_save :update_name_downcase!
+
   def to_s
     name
+  end
+
+  private
+
+  def update_name_downcase!
+    self.name_downcase = name.downcase
   end
 end
