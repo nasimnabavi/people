a730dd0bf930a06cb643c5a9d396f3f75d939081
Kacper Goliński <kacper.golinski@netguru.co>
2015-09-03 11:53:50 +0200

    update user serialised
    [#101674164]

--- app/serializers/api/v2/user_serializer.rb
+++ app/serializers/api/v2/user_serializer.rb
@@ -3,5 +3,9 @@ module Api::V2
     attributes :uid, :email, :first_name, :last_name, :gh_nick, :archived
 
     has_many :memberships, serializer: MembershipSerializer
+
+    def memberships
+      object.memberships.group_by(&:project_id).map { |_key, value| value.last }
+    end
   end
 end
--- spec/serializers/api/v2/user_serializer_spec.rb
+++ spec/serializers/api/v2/user_serializer_spec.rb
@@ -17,5 +17,26 @@ describe Api::V2::UserSerializer do
     it 'returns membership with project name' do
       expect(hash[:memberships][0][:project_name]).to eq(project.name)
     end
+
+    context 'when there are two memberships with same project' do
+      let!(:some_user) { create(:user) }
+      let!(:project) { create(:project) }
+      # let(:membership_data) { { user: some_user, project: project } }
+      let!(:membership1) do
+        create(:membership, user: some_user, project: project,
+          starts_at: 6.months.ago, ends_at: 4.months.ago)
+      end
+      let!(:membership2) do
+        create(:membership, user: some_user, project: project,
+          starts_at: 3.months.ago, ends_at: 1.months.ago)
+      end
+      let(:hash) { described_class.new(some_user).serializable_hash }
+
+      it 'returns only one, newset membership' do
+        expect(hash[:memberships][0][:starts_at]).to eq(membership2.starts_at)
+        expect(hash[:memberships][0][:ends_at]).to eq(membership2.ends_at)
+        expect(hash[:memberships][0][:role]).to eq(membership2.role.name)
+      end
+    end
   end
 end
