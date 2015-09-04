e6602e1344d280eb1ad38cd6122667cb918af93b
Sebastian Brych <sebastian.brych@netguru.pl>
2015-09-04 12:08:19 +0200

    Add next-project column to internals
    [#102734380]

--- app/services/scheduling/column_sets_builder.rb
+++ app/services/scheduling/column_sets_builder.rb
@@ -4,7 +4,7 @@ module Scheduling
       'all' => %w(user role current-project from-to next-project booked notes),
       'juniors-interns' => %w(user role current-project from-to next-project notes),
       'to-rotate' => %w(user role current-project from-to notes),
-      'internals' => %w(user role current-project from-to notes),
+      'internals' => %w(user role current-project from-to next-project notes),
       'in-progress' => %w(user role current-project from-to next-project notes),
       'in-commercial-with-due-date' => %w(user role current-project from-to notes),
       'booked' => %w(user role current-project booked notes),
