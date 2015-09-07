826fae259f054e6b3f92aad70d03ed14200ae68e
Sebastian Brych <sebastian.brych@netguru.pl>
2015-09-07 09:13:59 +0200

    Update spec for primary roles
    [#100026306]

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -3,7 +3,8 @@ require "spec_helper"
 describe "Users page", js: true do
   let(:role) { create(:role) }
   let(:user) { create(:user) }
-  let!(:developer) { create(:user, primary_role: role) }
+  let!(:developer) { create(:user) }
+  let!(:position) { create(:position, user: developer, role: role, primary: true) }
 
   before(:each) do
     page.set_rack_session 'warden.user.user.key' => User.serialize_into_session(user).unshift('User')
@@ -11,6 +12,7 @@ describe "Users page", js: true do
   end
 
   it "shows user role" do
-    expect(page).to have_content(developer.primary_role.name)
+    names = developer.primary_roles.pluck(:name).join(', ')
+    expect(page).to have_content(names)
   end
 end
