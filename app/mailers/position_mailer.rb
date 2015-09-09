31d7c125e3881772bf22c5ee011e7d7f423981ee
Dominik Wojciechowski <dominik.wojciechowski@netguru.co>
2015-09-09 10:41:31 +0200

    Add mailer for position creation
    [#100417518]

--- app/mailers/position_mailer.rb
+++ app/mailers/position_mailer.rb
@@ -1,2 +1,11 @@
 class PositionMailer < BaseMailer
+  def new_position(position, current_user)
+    @user = position.user.decorate
+    @role = position.role.name
+    @since = position.starts_at.to_date
+    @primary = position.primary
+    to = mail_receivers(current_user, position.user)
+    mail(to: to, subject: "#{@user.name} has been assigned a new role - #{@role}", project: @project)
+  end
+
 end
