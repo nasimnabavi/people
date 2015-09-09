68528e20ca3f703a1ce3ebca71e96922ce031028
Dominik Wojciechowski <dominik.wojciechowski@netguru.co>
2015-09-09 10:36:51 +0200

    Add mailer for membership update
    [#100417518]

--- app/mailers/membership_mailer.rb
+++ app/mailers/membership_mailer.rb
@@ -7,4 +7,13 @@ class MembershipMailer < BaseMailer
     mail(to: to, subject: "#{@user.name} has been added to #{@project.name}", project: @project)
   end
 
+  def updated(data, current_user)
+    @membership = data[:membership]
+    @user = @membership.user.decorate
+    @project = @membership.project
+    @old_values = data[:old_values]
+    to = mail_receivers(current_user, @user)
+    mail(to: to, subject: "Time span for #{@user.name} in #{@project.name} has been changed", project: @project)
+  end
+
 end
