a57ddbdc5563c01d27dc58e689d6cce689e1be68
Dominik Wojciechowski <dominik.wojciechowski@netguru.co>
2015-09-09 10:29:15 +0200

    Add mailer for membership creation
    [#100417518]

--- app/mailers/membership_mailer.rb
+++ app/mailers/membership_mailer.rb
@@ -1,2 +1,10 @@
 class MembershipMailer < BaseMailer
+  def created(membership, current_user)
+    @user = membership.user.decorate
+    @project = membership.project
+    @since = membership.starts_at
+    to = mail_receivers(current_user, @user)
+    mail(to: to, subject: "#{@user.name} has been added to #{@project.name}", project: @project)
+  end
+
 end
