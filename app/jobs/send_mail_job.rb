7a5cbd7579ce366179311d6ba883a182f022278c
Dominik Wojciechowski <dominik.wojciechowski@netguru.co>
2015-09-09 10:34:35 +0200

    Add method that receives current user
    [#100417518]

--- app/jobs/send_mail_job.rb
+++ app/jobs/send_mail_job.rb
@@ -4,4 +4,8 @@ class SendMailJob
   def perform(object, method, param)
     object.send(method, param).deliver
   end
+
+  def perform_with_user(object, method, param, current_user)
+    object.send(method, param, current_user).deliver
+  end
 end
