bc5d12399a6510b25dd36585d60b55421693fb25
Dominik Wojciechowski <dominik.wojciechowski@netguru.co>
2015-09-10 10:37:36 +0200

    Fix missing space
    [2af814c58dce2525f973758ff211877d3d18cfbd] [#100417518]

--- app/mailers/base_mailer.rb
+++ app/mailers/base_mailer.rb
@@ -1,7 +1,7 @@
 class BaseMailer < ActionMailer::Base
   default from: AppConfig.emails.from
 
-  def mail_receivers(current_user, user=nil)
+  def mail_receivers(current_user, user = nil)
     (AppConfig.emails.admin + [user.try(:email)] - [current_user.email]).compact
   end
 end
