ffe5c06eeb704f4e6373bf99ee7b0d314d467c24
Kacper Goliński <kacper.golinski@netguru.co>
2015-09-10 10:36:10 +0200

    remove unnecessary code
    It was causing that factories was required twice, and was throwing an
    error.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,6 @@
 require 'factory_girl'
 require 'faker'
 
-Dir[Rails.root.join("spec/factories/*.rb")].each {|f| require f}
-
 billable_roles = %w(senior developer)
 non_billable_roles = %w(junior praktykant pm junior\ pm qa junior\ qa)
 technical_roles = %w(junior praktykant developer senior)
