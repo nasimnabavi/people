37d4a184c1f0b5189bdac3d2b31f6fbc428d3ec5
Dorian Sawa <dorian.sawa@netguru.pl>
2015-10-06 15:15:04 +0200

    Not scheduled should show only users with technical role as primary
    [#103081102]

--- app/services/scheduling/missing_users.rb
+++ app/services/scheduling/missing_users.rb
@@ -3,8 +3,9 @@ module Scheduling
     def initialize(user_ids)
       @user_ids = user_ids
     end
+
     def call
-      User.technical.active.where.not(id: @user_ids)
+      User.technical.where(positions: { primary: true }).active.where.not(id: @user_ids)
     end
   end
 end
