163751931ea256d510139a2c13f50597eb907579
Mateusz Palyz <mateusz.palyz@netguru.pl>
2015-10-12 20:50:29 +0200

    add empty statistics component test [#104991676]

---
+++ app/react/components/statistics-test.jsx
@@ -0,0 +1,11 @@
+import React from 'react/addons';
+const TestUtils = React.addons.TestUtils;
+import expect from 'expect';
+import Statistics from './statistics';
+
+describe('Statistics', () => {
+
+  it('renders', () => {
+
+  });
+});
