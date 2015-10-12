818d618324d4d182da8c50dd5d57bb2b5fbc1b4a
Mateusz Palyz <mateusz.palyz@netguru.pl>
2015-10-12 14:56:58 +0200

    register statistics component

--- app/react/index.js
+++ app/react/index.js
@@ -1,6 +1,7 @@
 window.React = require('react');
 
 import HelloWorld from './components/hello-world';
+import Statistics from './components/statistics';
 
 registerComponent('hello-world', HelloWorld);
-
+registerComponent('statistics', Statistics);
