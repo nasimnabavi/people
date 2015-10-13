7e17edf6da5b773e728aa82fbdaead19d9768f6f
Mateusz Palyz <mateusz.palyz@netguru.pl>
2015-10-13 11:11:57 +0200

    add statistics controller spec [#104991676]

---
+++ spec/controllers/api/v2/statistics_controller_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe Api::V2::StatisticsController do
+  describe 'GET #index' do
+    context 'without api token' do
+      it 'returns response status 403' do
+        get :index
+        expect(response.status).to eq 403
+      end
+    end
+
+    context 'with api token' do
+      let(:token) { AppConfig.api_token }
+      let(:statistics) do
+        {
+          "commercialProjectsNumber" => 2,
+          "internalProjectsNumber" => 1,
+          "projectsEndingThisMonthNumber" => 0,
+          "beginningNextMonthProjectsNumber" => 1
+        }
+      end
+
+      before do
+        create(:project, :internal, starts_at: '2015-10-2', end_at: '2015-11-2')
+        create(:project, :commercial, starts_at: '2015-10-4', end_at: nil)
+        create(:project, :commercial, starts_at: '2015-10-8', end_at: '2015-11-8')
+        create(:project, :commercial, :potential, starts_at: '2015-11-7', end_at: '2015-12-30')
+        get :index, token: token, date: '2015-10-1'
+      end
+
+      it 'returns response status 200' do
+        expect(response.status).to eq 200
+      end
+
+      it 'returns proper json' do
+        expect(json_response).to eq statistics
+      end
+    end
+  end
+end
