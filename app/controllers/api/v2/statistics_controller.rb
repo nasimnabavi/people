87bb493a2c13b0f41333ac06e21a25b395f8bd6b
Mateusz Palyz <mateusz.palyz@netguru.pl>
2015-10-27 10:55:39 +0100

    use startDate and endDate in statistics controller [#106312496]

--- app/controllers/api/v2/statistics_controller.rb
+++ app/controllers/api/v2/statistics_controller.rb
@@ -5,19 +5,18 @@ module Api::V2
 
     before_filter :authenticate_admin!
 
-    expose(:commercial_projects) { Project.commercial_between(beginning_of_month, end_of_month) }
-    expose(:internal_projects) { Project.internal_between(beginning_of_month, end_of_month) }
-    expose(:projects_ending_this_month) { Project.ends_between(beginning_of_month, end_of_month) }
-    expose(:beginning_next_month_projects) do
-      Project.beginning_between(beginning_of_next_month, end_of_next_month)
-    end
+    expose(:commercial_projects) { Project.commercial_between(start_date, end_date) }
+    expose(:internal_projects) { Project.internal_between(start_date, end_date) }
+    expose(:projects_ending_this_month) { Project.ends_between(start_date, end_date) }
+    expose(:beginning_next_month_projects) { Project.beginning_between(today, thirty_days_from_today) }
 
     expose(:billable_developers) { [] }
     expose(:developers_in_internals) { [] }
     expose(:juniors_and_interns) { [] }
 
     def index
-      @date = Time.zone.parse("#{statistics_params[:date]}-1")
+      @start_date = Time.zone.parse(statistics_params[:startDate])
+      @end_date = Time.zone.parse(statistics_params[:endDate])
       render json: statistics_json
     end
 
@@ -36,7 +35,7 @@ module Api::V2
     end
 
     def statistics_params
-      params.permit(:token, :date)
+      params.permit(:token, :startDate, :endDate)
     end
   end
 end
--- app/controllers/concerns/dates_management.rb
+++ app/controllers/concerns/dates_management.rb
@@ -1,23 +1,13 @@
 module DatesManagement
   extend ActiveSupport::Concern
 
-  def beginning_of_month
-    @date.beginning_of_month
+  attr_reader :start_date, :end_date
+
+  def today
+    Time.now.beginning_of_day
   end
 
-  def end_of_month
-    @date.end_of_month
-  end
-
-  def beginning_of_next_month
-    next_month.beginning_of_month
-  end
-
-  def end_of_next_month
-    next_month.end_of_month
-  end
-
-  def next_month
-    @date.next_month
+  def thirty_days_from_today
+    today + 30.days
   end
 end
