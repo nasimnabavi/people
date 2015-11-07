92b4a957a55102e8aa93040b5c2cc84c914ece33
Mateusz Palyz <mateusz.palyz@netguru.pl>
2015-11-07 19:52:10 +0100

    update tests after switching to rabl in statistics controller

--- spec/controllers/api/v2/statistics_controller_spec.rb
+++ spec/controllers/api/v2/statistics_controller_spec.rb
@@ -11,6 +11,9 @@ describe Api::V2::StatisticsController do
 
     context 'with api token' do
       let(:token) { AppConfig.api_token }
+      let(:params) do
+        { format: :json, token: token, startDate: '2015-10-1', endDate: '2015-10-30' }
+      end
 
       before do
         create(:project, :internal, starts_at: '2015-10-2', end_at: '2015-11-2')
@@ -23,15 +26,17 @@ describe Api::V2::StatisticsController do
         before { sign_in create(:user) }
 
         it 'returns response status 403' do
-          get :index, token: token, startDate: '2015-10-1', endDate: '2015-10-30'
+          get :index, params
           expect(response.status).to eq 403
         end
       end
 
       context 'with admin' do
+        render_views
+
         before do
           sign_in create(:user, :admin)
-          get :index, token: token, startDate: '2015-10-1', endDate: '2015-10-30'
+          get :index, params
         end
 
         it 'returns response status 200' do
@@ -41,8 +46,8 @@ describe Api::V2::StatisticsController do
         it 'returns proper json' do
           expect(json_response['commercialProjects'].length).to eq 2
           expect(json_response['internalProjects'].length).to eq 1
-          expect(json_response['projectsEndingThisMonth'].length).to eq 0
-          expect(json_response['beginningNextMonthProjects'].length).to eq 1
+          expect(json_response['projectsEndingBetween'].length).to eq 0
+          expect(json_response['beginningSoonProjects'].length).to eq 1
         end
       end
     end
