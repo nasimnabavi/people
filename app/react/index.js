2905b30530eac38de3ea5b2f36bbca92a3c6dcc3
Bartosz Łęcki <bartosz.lecki@netguru.pl>
2015-11-09 11:19:47 +0100

    Register teams as a react component
    Resolves: [#106308578]

--- app/react/index.js
+++ app/react/index.js
@@ -1,5 +1,7 @@
 window.React = require('react');
 
 import Statistics from './components/statistics';
+import Teams from './components/teams/teams';
 
 registerComponent('statistics', Statistics);
+registerComponent('teams', Teams);
