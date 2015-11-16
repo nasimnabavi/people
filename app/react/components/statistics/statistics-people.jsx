2d34027e775be94d35300a8c0eeda9930325b51d
Bartosz Łęcki <bartosz.lecki@netguru.pl>
2015-11-16 15:52:01 +0100

    Add StatisticsPeople component
    Resolves: [#106313280]

---
+++ app/react/components/statistics/statistics-people.jsx
@@ -0,0 +1,48 @@
+import React, {PropTypes} from 'react';
+import StatisticsPeopleRow from './statistics-people-row';
+
+export default class StatisticsPeople extends React.Component {
+  render() {
+    return(
+      <div>
+        <h3>Positions</h3>
+        <table className='table'>
+          <thead>
+            <tr>
+              <th>Technology/Role</th>
+              <th>Interns</th>
+              <th>Juniors</th>
+              <th>Regular</th>
+              <th>Senior</th>
+              <th>Sum</th>
+            </tr>
+          </thead>
+          <tbody>
+            <StatisticsPeopleRow
+              label={"Ruby on Rails"}
+              interns={this.props.statistics.interns}
+              juniors={this.props.statistics.juniorRor}
+              regulars={this.props.statistics.rorDevs}
+              seniors={this.props.statistics.seniorRorDevs} />
+            <StatisticsPeopleRow
+              label={"iOS"}
+              interns={[]}
+              juniors={this.props.statistics.juniorIos}
+              regulars={this.props.statistics.iosDevs}
+              seniors={this.props.statistics.seniorIosDevs} />
+            <StatisticsPeopleRow
+              label={"Android"}
+              interns={[]}
+              juniors={this.props.statistics.juniorAndroid}
+              regulars={this.props.statistics.androidDevs}
+              seniors={this.props.statistics.seniorAndroidDevs} />
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+StatisticsPeople.propTypes = {
+  statistics: PropTypes.array.required
+};
