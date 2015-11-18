351ea2add922fafbf8e2dfcdfba28b0b9793c5fe
Bartosz Łęcki <bartosz.lecki@netguru.pl>
2015-11-18 17:39:55 +0100

    Add actions and store for users
    Resolves: [#108345074]

---
+++ app/react/actions/UserActions.js
@@ -0,0 +1,13 @@
+import alt from '../alt';
+
+class UserActions {
+  constructor() {
+    this.generateActions(
+      'addToTeam',
+      'promoteToLeader',
+      'removeFromTeam'
+    )
+  }
+}
+
+export default alt.createActions(UserActions);
---
+++ app/react/stores/UserStore.js
@@ -0,0 +1,57 @@
+import alt from '../alt';
+
+import UserActions from '../actions/UserActions';
+
+class UserStore {
+  constructor() {
+    this.bindActions(UserActions);
+    this.users = [];
+  }
+
+
+  addToTeam(params) {
+    let user = this.users.filter(user => user.id == params.userId)[0];
+    let userIndex = this.users.indexOf(user);
+    if(user.team_ids === null) {
+      user.team_ids = [params.teamId];
+    } else { user.team_ids.push(params.teamId) };
+    let userPersisted = () => {
+      Messenger().success(`User ${user.name} added to team`);
+      this.users[userIndex] = user;
+      this.emitChange();
+    };
+    let failedToAddUserToTeam = () => {
+      Messenger().success(`Failed to add user ${user.name} to team`);
+    }
+    this._updateUser(user, userPersisted, failedToAddUserToTeam);
+    return false;
+  }
+
+  _updateUser(user, onSuccess, onFail) {
+    $.ajax({
+      url: Routes.user_path(user.id),
+      type: "PUT",
+      dataType: 'json',
+      data: {
+        user: {
+          team_ids: user.team_ids,
+          leader_team_id: user.leader_team_id
+        }
+      }
+    }).done(onSuccess).fail(onFail);
+  }
+
+  promoteToLeader(team_id) {
+    // CODE
+  }
+
+  removeFromTeam(team_id) {
+    // CODE
+  }
+
+  static setInitialState(users) {
+    this.state.users = users;
+  }
+}
+
+export default alt.createStore(UserStore);
