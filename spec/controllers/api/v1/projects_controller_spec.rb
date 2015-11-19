cd1df660dac4a8be5ea21dd82aa787f9639ff19c
Bartosz Łęcki <bartosz.lecki@netguru.pl>
2015-11-19 08:46:33 +0100

    Change specs for projects controller
    Resolves: [#108427704]

--- spec/controllers/api/v1/projects_controller_spec.rb
+++ spec/controllers/api/v1/projects_controller_spec.rb
@@ -5,6 +5,7 @@ describe Api::V1::ProjectsController do
 
   let!(:project1) { create(:project, potential: false) }
   let!(:project2) { create(:project, potential: true) }
+  let!(:project3) { create(:project, potential: false, synchronize: false) }
   let(:project_keys) { %w(id name archived potential toggl_bookmark slug) }
 
   describe 'GET #index' do
@@ -23,6 +24,12 @@ describe Api::V1::ProjectsController do
     it 'does not return potential projects' do
       expect(json_response.length).to eq(1)
       expect(json_response[0]['name']).to eq(project1.name)
+    end
+
+    it 'does not return project which should not be synchronized' do
+      expect(json_response.length).to eq(1)
+      project_names = json_response.map { |project| project['name'] }
+      expect(project_names).not_to include project3.name
     end
   end
 end
