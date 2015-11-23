855b64e1924a1c0bb061cf41d4be31b665f3772e
Bartosz Łęcki <bartosz.lecki@netguru.pl>
2015-11-23 10:54:36 +0100

    Decorates memberships for role
    Resolves: [#108615048]

--- app/decorators/role_decorator.rb
+++ app/decorators/role_decorator.rb
@@ -2,6 +2,7 @@ class RoleDecorator < Draper::Decorator
 
   delegate_all
   decorates_association :users, scope: :technical_active
+  decorates_association :memberships
 
   def label
     h.content_tag :span, name, class: "label label-default", style: "background-color: #{color}"
