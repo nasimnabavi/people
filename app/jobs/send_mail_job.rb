86d24cb7840aaf14807a45dbf979e3a30b5c32d2
Bartosz Łęcki <bartosz.lecki@netguru.pl>
2015-11-26 08:49:47 +0100

    Change deliver to deliver_now to remove deprecation
    Resolves: [#106952070]

--- app/jobs/send_mail_job.rb
+++ app/jobs/send_mail_job.rb
@@ -2,10 +2,10 @@ class SendMailJob
   include SuckerPunch::Job
 
   def perform(object, method, param)
-    object.send(method, param).deliver
+    object.send(method, param).deliver_now
   end
 
   def perform_with_user(object, method, param, current_user)
-    object.send(method, param, current_user).deliver
+    object.send(method, param, current_user).deliver_now
   end
 end
