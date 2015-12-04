9e72c7315ceb725fc36dedbdf1053e4cc75b7bee
Bartosz Łęcki <bartosz.lecki@netguru.pl>
2015-12-04 10:40:30 +0100

    Add Filter store and actions
    Resolves: [#109139328]

---
+++ app/react/stores/FilterStore.js
@@ -0,0 +1,36 @@
+import alt from '../alt';
+
+import FilterActions from '../actions/FilterActions';
+
+class FilterStore {
+  constructor() {
+    this.bindActions(FilterActions);
+    this.projectIds = [];
+    this.userIds = [];
+    this.showNext = false;
+    this.highlightEnding = false;
+    this.highlightNotBillable = false;
+  }
+
+  changeProjectFilter(projectIds) {
+    this.setState({ projectIds: projectIds});
+  }
+
+  changeUserFilter(userIds) {
+    this.setState({ userIds: userIds });
+  }
+
+  highlightEnding(value) {
+    this.setState({ highlightEnding: value });
+  }
+
+  showNext(value) {
+    this.setState({ showNext: value });
+  }
+
+  highlightNotBillable(value) {
+    this.setState({ highlightNotBillable: value });
+  }
+}
+
+export default alt.createStore(FilterStore);
