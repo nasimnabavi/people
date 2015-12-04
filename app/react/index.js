a4704b8537f638cde4176c4b98e2455965e38acb
Bartosz Łęcki <bartosz.lecki@netguru.pl>
2015-12-04 10:16:52 +0100

    Register component projects
    Resolves: [#109139328]

--- app/react/index.js
+++ app/react/index.js
@@ -5,6 +5,8 @@ window.ReactDOM = ReactDOM;
 
 import Statistics from './components/statistics/statistics';
 import Teams from './components/teams/teams';
+import Projects from './components/projects/projects';
 
 registerComponent('statistics', Statistics);
 registerComponent('teams', Teams);
+registerComponent('projects', Projects);
