5ca8456c37d7845613f48a7826e7d078386462d3
Jacek Jakubik <jacek.jakubik@netguru.pl>
2015-12-10 11:25:17 +0100

    do not link bin

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :deploy_to, ENV['DEPLOY_PATH']
 set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
 
 set :linked_files, %w(config/mongoid.yml config/sec_config.yml config/database.yml)
-set :linked_dirs, %w(bin log tmp vendor/bundle public/uploads node_modules)
+set :linked_dirs, %w(log tmp vendor/bundle public/uploads node_modules)
 set :npm_flags, '--production --no-spin'
 
 namespace :webpack do
