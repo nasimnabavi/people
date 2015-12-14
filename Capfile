f3dc6e5a9203677f7d1c501fc87dc834fc580142
Jacek Jakubik <jacek.jakubik@netguru.pl>
2015-12-14 15:22:50 +0100

    Deploy staging using Docker

--- Capfile
+++ Capfile
@@ -1,8 +1,21 @@
 require 'capistrano/setup'
 require 'capistrano/deploy'
-require 'capistrano/bundler'
-require 'capistrano/npm'
-require 'capistrano/rails'
-require 'capistrano/passenger'
-require 'rvm1/capistrano3'
-require 'whenever/capistrano'
+
+task :docker do
+  require 'capistrano/docker'
+  require 'capistrano/docker/npm'
+  require 'capistrano/docker/assets'
+  require 'capistrano/docker/migration'
+end
+
+task :non_docker do
+  require 'capistrano/bundler'
+  require 'capistrano/npm'
+  require 'capistrano/rails'
+  require 'capistrano/passenger'
+  require 'rvm1/capistrano3'
+  require 'whenever/capistrano'
+end
+
+task 'staging' => [:docker]
+task 'production' => [:non_docker]
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,3 +2,21 @@ require 'net/ssh/proxy/command'
 server ENV['PRODUCTION_SERVER'], user: ENV['PRODUCTION_USER'], roles: %w{web app db}
 set :ssh_options, proxy: Net::SSH::Proxy::Command.new("ssh #{ENV['GATEWAY']} -W %h:%p")
 set :branch, 'production'
+
+set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
+
+set :linked_files, %w(config/mongoid.yml config/sec_config.yml config/database.yml)
+set :linked_dirs, %w(log tmp vendor/bundle public/uploads node_modules)
+set :npm_flags, '--production --no-spin'
+
+namespace :webpack do
+  task :build do
+    on roles :web do
+      within release_path do
+        execute :bundle, 'exec npm run build'
+      end
+    end
+  end
+end
+
+after 'npm:install', 'webpack:build'
