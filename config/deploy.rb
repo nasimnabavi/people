ae120183fe3853db698da41ab03d29b124db33e9
Jacek Jakubik <jacek.jakubik@netguru.pl>
2015-12-14 15:22:50 +0100

    Deploy staging using Docker

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,20 +4,3 @@ set :application, "people"
 set :repo_url,  "git://github.com/netguru/people.git"
 set :deploy_to, ENV['DEPLOY_PATH']
 
-set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
-
-set :linked_files, %w(config/mongoid.yml config/sec_config.yml config/database.yml)
-set :linked_dirs, %w(bin log tmp vendor/bundle public/uploads node_modules)
-set :npm_flags, '--production --no-spin'
-
-namespace :webpack do
-  task :build do
-    on roles :web do
-      within release_path do
-        execute :bundle, 'exec npm run build'
-      end
-    end
-  end
-end
-
-after 'npm:install', 'webpack:build'
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,2 +1,28 @@
 set :branch, 'master'
 server ENV['STAGING_SERVER'], user: ENV['STAGING_USER'], roles: %w{web app db}
+
+set :docker_volumes, [
+  "#{shared_path}/config/sec_config.yml:/var/www/app/config/sec_config.yml",
+  "#{shared_path}/log:/var/www/app/log",
+  "#{shared_path}/public/uploads:/var/www/app/public/uploads",
+  "people_staging_assets:/var/www/app/public/assets",
+  "people_staging_node_modules:/var/www/app/node_modules",
+  "#{shared_path}/assets/javascripts/react_bundle.js:/var/www/app/app/assets/javascripts/react_bundle.js",
+]
+
+set :docker_links, %w(postgres_ambassador:postgres)
+set :docker_additional_options, -> { "--env-file #{shared_path}/.env" }
+set :docker_apparmor_profile, "docker-ptrace"
+set :docker_dockerfile, "docker/staging/Dockerfile"
+
+namespace :docker do
+  namespace :npm do
+    task :build do
+      on roles(fetch(:docker_role)) do
+        execute :docker, task_command("npm run build")
+      end
+    end
+  end
+end
+
+after "docker:npm:install", "docker:npm:build"
