8c9bbd3a82812e298c38ae768d500f1ad3afca8e
Piotr Pawlik <piotr.pawlik@netguru.pl>
2015-12-22 15:47:10 +0100

    Added how it works modal
    [#109139328]

--- app/react/components/scheduling/filters.jsx
+++ app/react/components/scheduling/filters.jsx
@@ -1,11 +1,13 @@
 import React, {PropTypes} from 'react';
 import FiltersDropdowns from './filters-dropdowns';
+import HowItWorksModal from './how-it-works-modal';
 
 export default class Filters extends React.Component {
   render() {
     return(
       <div id="filters">
         <FiltersDropdowns />
+        <HowItWorksModal />
       </div>
     );
   }
