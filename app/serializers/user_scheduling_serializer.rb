685d49098704af79de4c6bfea1b164ea62cd571c
Piotr Pawlik <piotr.pawlik@netguru.pl>
2015-12-22 14:37:46 +0100

    Added needed serialisers
    ability, role and user_scheduling
    [#109139328]

---
+++ app/serializers/user_scheduling_serializer.rb
@@ -0,0 +1,15 @@
+class UserSchedulingSerializer < ActiveModel::Serializer
+  attributes :id, :gravatar, :primary_role, :name, :user_notes
+  has_many :current_memberships, embed: :objects, serializer: MembershipSchedulingSerializer
+  has_many :next_memberships, embed: :objects, serializer: MembershipSchedulingSerializer
+  has_many :booked_memberships, embed: :objects, serializer: MembershipSchedulingSerializer
+
+  def name
+    "#{object.last_name} #{object.first_name}"
+  end
+
+  def primary_role
+    role = object.primary_roles[0]
+    { name: role.try(:name), id: role.try(:id) }
+  end
+end
