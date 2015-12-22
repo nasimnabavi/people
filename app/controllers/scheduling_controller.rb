4cff45b4cf12e534b8daaafdd16ec1f8f1b406b1
Piotr Pawlik <piotr.pawlik@netguru.pl>
2015-12-22 14:36:09 +0100

    Added technical for scheduled_user_repository
    [#109139328]

--- app/controllers/scheduling_controller.rb
+++ app/controllers/scheduling_controller.rb
@@ -1,5 +1,5 @@
 class SchedulingController < ApplicationController
-  expose(:users) { ActiveModel::ArraySerializer.new(UserRepository.new.active_minimal,
+  expose(:users) { ActiveModel::ArraySerializer.new(ScheduledUsersRepository.new.technical,
     each_serializer: UserSchedulingSerializer).as_json }
   expose(:roles) { ActiveModel::ArraySerializer.new(RolesRepository.new.all_technical,
     each_serializer: RoleSerializer).as_json }
