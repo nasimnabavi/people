92922325ad9f01b28d74864ce7c2c4bac42dd468
smnkrt <szymon.krywult@netguru.pl>
2016-01-18 10:53:40 +0100

    add constants for FilterTabs

---
+++ app/react/constants/scheduling/FilterTabs.js
@@ -0,0 +1,8 @@
+module.exports = {
+  ALL: "show_all",
+  JUNIORS_AND_INTERNS: "show_juniors_and_interns",
+  TO_ROTATE: "show_to_rotate",
+  INTERNALS: "show_internals",
+  IN_ROTATION: "show_in_rotation",
+  UNAVAILABLE: "show_unavailable"
+};
