5bd6ec2ebefa51b2f9318bece8fd91b150f83293
smnkrt <szymon.krywult@netguru.pl>
2016-01-20 15:38:02 +0100

    update filterTabs constants values

--- app/react/constants/scheduling/FilterTabs.js
+++ app/react/constants/scheduling/FilterTabs.js
@@ -1,9 +1,11 @@
 module.exports = {
-  ALL: "show_all",
-  JUNIORS_AND_INTERNS: "show_juniors_and_interns",
-  TO_ROTATE: "show_to_rotate",
-  INTERNALS: "show_internals",
-  IN_ROTATION: "show_in_rotation",
-  UNAVAILABLE: "show_unavailable",
+  ALL: "all",
+  JUNIORS_AND_INTERNS: "juniors_and_interns",
+  TO_ROTATE: "to_rotate",
+  INTERNALS: "in_internals",
+  IN_ROTATION: "with_rotations_in_progress",
+  IN_COMMERCIAL_PROJECTS_WITH_DUE_DATE: "in_commercial_projects_with_due_date",
+  BOOKED: "booked",
+  UNAVAILABLE: "unavailable",
   NOT_SCHEDULED: "not_scheduled"
 };
