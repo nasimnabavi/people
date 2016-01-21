dfac568b9e3ecd3ed65d1bced1c9f8caea13df07
smnkrt <szymon.krywult@netguru.pl>
2016-01-21 13:21:10 +0100

    extract common spec cases into shared example
    for sheduling_controller_spec

---
+++ spec/support/shared_examples/scheduling_controller_shared.rb
@@ -0,0 +1,36 @@
+shared_examples 'user is an admin and response is 200' do |action|
+  let!(:developer) { create(:developer_in_project) }
+  let!(:pm) { create(:pm_user) }
+
+  context 'when current user is an admin' do
+    let(:admin_user) { create(:user, :admin) }
+
+    before { sign_in(admin_user) }
+
+    it 'responds successfully with an HTTP 200 status code' do
+      get action
+      expect(response).to be_success
+      expect(response.status).to eq(200)
+    end
+
+    it 'displays users on view' do
+      get action
+      expect(response.body).to match(developer.last_name)
+      expect(response.body).not_to match(pm.last_name)
+    end
+  end
+end
+
+shared_examples 'user is not an admin and response is 200' do |action|
+  context 'when current user is not an admin' do
+    let(:user) { create(:user) }
+
+    before { sign_in(user) }
+
+    it 'responds successfully with an HTTP 200 status code' do
+      get action
+      expect(response).to be_success
+      expect(response.status).to eq(200)
+    end
+  end
+end
