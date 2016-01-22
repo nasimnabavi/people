665c70d287f815b9a61558b4daa23e46a8846ef1
smnkrt <szymon.krywult@netguru.pl>
2016-01-22 16:32:56 +0100

    remove unused code from FilteringService

--- app/react/services/scheduling/FilteringService.js
+++ app/react/services/scheduling/FilteringService.js
@@ -1,5 +1,4 @@
 import Filters from './Filters';
-import Sorting from './Sorting';
 import * as FilterTabs from '../../constants/scheduling/FilterTabs';
 
 export default class FilteringService {
@@ -16,42 +15,6 @@ export default class FilteringService {
     }
     if(store.abilityIds.length !== 0) {
       usersToView = Filters.selectAbilities(usersToView, store);
-    }
-
-    switch(store.currentTab) {
-      case FilterTabs.ALL:
-        usersToView = Filters.selectAll(usersToView);
-      break;
-      case FilterTabs.JUNIORS_AND_INTERNS:
-        usersToView = Filters.selectJuniorsAndInterns(usersToView);
-      break;
-      case FilterTabs.TO_ROTATE:
-        usersToView = Filters.selectToRotate(usersToView);
-      break;
-      case FilterTabs.INTERNALS:
-        usersToView = Filters.selectInternals(usersToView);
-      break;
-      case FilterTabs.IN_ROTATION:
-        usersToView = Filters.selectInRotation(usersToView);
-      break;
-      case FilterTabs.IN_COMMERCIAL_PROJECTS_WITH_DUE_DATE:
-        usersToView = Filters.showInCommercialProjectsWithDueDate(usersToView);
-      break;
-      case FilterTabs.BOOKED:
-        usersToView = Filters.showBooked(usersToView);
-      break;
-      case FilterTabs.UNAVAILABLE:
-        usersToView = Filters.selectUnavailable(usersToView);
-      break;
-      case FilterTabs.NOT_SCHEDULED:
-        usersToView = Filters.selectNotScheduled(usersToView);
-      break;
-    }
-
-    if(_.contains([FilterTabs.ALL, FilterTabs.NOT_SCHEDULED], store.currentTab)) {
-      usersToView = Sorting.sortUsersAlphabetically(usersToView);
-    } else {
-      usersToView = Sorting.sortByTimeInProject(usersToView);
     }
 
     return usersToView;
