b37734b9d8b6142010b22842a7cfaf1652a4c800
smnkrt <szymon.krywult@netguru.pl>
2016-01-22 16:35:55 +0100

    add helper method in UserSchedulingSerializer
    for computing class of rotation priority

--- app/serializers/user_scheduling_serializer.rb
+++ app/serializers/user_scheduling_serializer.rb
@@ -1,5 +1,5 @@
 class UserSchedulingSerializer < ActiveModel::Serializer
-  attributes :id, :gravatar, :primary_role, :name, :user_notes, :ability_ids
+  attributes :id, :gravatar, :primary_role, :name, :user_notes, :ability_ids, :rotation_class
   has_many :current_memberships, embed: :objects, serializer: MembershipSchedulingSerializer
   has_many :next_memberships, embed: :objects, serializer: MembershipSchedulingSerializer
   has_many :booked_memberships, embed: :objects, serializer: MembershipSchedulingSerializer
@@ -11,5 +11,13 @@ class UserSchedulingSerializer < ActiveModel::Serializer
   def primary_role
     role = object.primary_roles[0]
     { name: role.try(:name), id: role.try(:id) }
+  end
+
+  def rotation_class
+    membership_start_date = object.longest_current_membership ? object.longest_current_membership.starts_at : Date.today
+    months_in_project = (Date.today - membership_start_date)/30.to_f
+    return 'rotation-needed-danger' if months_in_project >= 8.0
+    return 'rotation-needed-warning' if months_in_project >= 7.0
+    return 'rotation-needed' if months_in_project >= 6.0
   end
 end
