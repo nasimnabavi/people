7744d405bb97e163a97b50ada13f773ee1959e5b
smnkrt <szymon.krywult@netguru.pl>
2016-01-27 14:07:52 +0100

    remove locked version from js_stack in GEMFILE

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,8 @@
 //= require js_stack
 //= require js_stack/plugins/underscore.string
 //= require js_stack/plugins/underscore.inflections
-//= require moment
+//= require js_stack/plugins/backbone.associations
+//= require js_stack/plugins/moment
 //= require backbone/hrguru
 //= require abilities/icon_preview
 //= require abilities/selectize_skills
