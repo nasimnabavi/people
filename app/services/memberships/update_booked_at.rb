7181080af2c24bc750ab5c4474f6c9bb9138524a
smnkrt <szymon.krywult@netguru.pl>
2016-01-28 16:11:57 +0100

    store time of booking taking place

--- app/services/memberships/update_booked_at.rb
+++ app/services/memberships/update_booked_at.rb
@@ -5,8 +5,19 @@ module Memberships
     end
 
     def call
-      booked_date = @membership.booked ? @membership.starts_at : nil
+      booked_date = @membership.booked ? date_with_current_hour : nil
       @membership.booked_at = booked_date
+    end
+
+    private
+
+    def date_with_current_hour
+      starts_at = @membership.starts_at
+      Time.now.change(
+        year: starts_at.year,
+        month: starts_at.month,
+        day: starts_at.day
+      )
     end
   end
 end
