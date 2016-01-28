be87a4142ad8df96d1fab842ce3883d6a74bc8a6
smnkrt <szymon.krywult@netguru.pl>
2016-01-28 10:22:37 +0100

    update UpdateBookedAt spec

--- spec/services/memberships/update_booked_at_spec.rb
+++ spec/services/memberships/update_booked_at_spec.rb
@@ -22,16 +22,16 @@ describe Memberships::UpdateBookedAt do
           membership.booked = true
         end
 
-        it 'changes booked_at to current time' do
+        it 'changes booked_at to starts_at date' do
           subject
-          expect(membership.booked_at.utc.to_s).to eq Time.zone.now.utc.to_s
+          expect(membership.booked_at.to_date).to eq membership.starts_at
         end
       end
     end
 
     context 'booked has not changed' do
       it 'does not change booked_at' do
-        expect{ subject }.not_to change { membership.reload.booked_at }
+        expect { subject }.not_to change { membership.reload.booked_at }
       end
     end
   end
