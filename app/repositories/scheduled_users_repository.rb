32ae8521883122eaee7e20d77136865b613d7bb3
smnkrt <szymon.krywult@netguru.pl>
2016-02-01 16:10:55 +0100

    sort to_rotate with sort method instead of db query

--- app/repositories/scheduled_users_repository.rb
+++ app/repositories/scheduled_users_repository.rb
@@ -26,7 +26,6 @@ class ScheduledUsersRepository
         projects: { end_at: nil, internal: false },
         memberships: { ends_at: nil })
       .merge(Project.active.nonpotential.not_maintenance)
-      .order('COALESCE(memberships.starts_at, projects.starts_at)')
   end
 
   def in_internals
