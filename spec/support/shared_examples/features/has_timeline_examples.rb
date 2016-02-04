7418beeb98dabb9d3fe591a81f92398f6679b5e1
smnkrt <szymon.krywult@netguru.pl>
2016-02-04 16:30:34 +0100

    try to pass specs with sleep, without new circle config

--- spec/support/shared_examples/features/has_timeline_examples.rb
+++ spec/support/shared_examples/features/has_timeline_examples.rb
@@ -1,11 +1,13 @@
 shared_examples 'has timeline visible' do
   it 'shows timeline' do
+    sleep 5
     expect(page).to have_css('.timeline .events')
   end
 end
 
 shared_examples 'has timeline event visible' do
   it 'shows timeline event' do
+    sleep 5
     expect(page).to have_css('.event .time')
   end
 end
