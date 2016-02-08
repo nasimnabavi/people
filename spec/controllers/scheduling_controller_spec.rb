1542971dffc128c121e56ae7a33f3047b3172bbe
smnkrt <szymon.krywult@netguru.pl>
2016-02-08 19:55:16 +0100

    update scheduling controller specs
    - add cache cleaning
    - uses dryed shared examples
    - use proper action names

--- spec/controllers/scheduling_controller_spec.rb
+++ spec/controllers/scheduling_controller_spec.rb
@@ -3,14 +3,16 @@ require 'spec_helper'
 describe SchedulingController do
   render_views
 
-  describe '#index' do
-    it_behaves_like 'user is an admin and response is 200', :index
-    it_behaves_like 'user is not an admin and response is 200', :index
+  before(:each) { Rails.cache.clear }
+
+  describe '#all' do
+    it_behaves_like 'user is an admin and response is 200', :all
+    it_behaves_like 'user is an admin and only technical users are visible', :all
+    it_behaves_like 'user is not an admin and response is 200', :all
   end
 
-
   describe '#not_scheduled' do
-    it_behaves_like 'user is an admin and response is 200', :index
+    it_behaves_like 'user is an admin and response is 200', :not_scheduled
 
     context 'when current user is not an admin redirection occurs' do
       let(:user) { create(:user) }
