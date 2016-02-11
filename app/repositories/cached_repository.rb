95cf04c50b96f257fb90ec9cb49c75d9adaae647
smnkrt <szymon.krywult@netguru.pl>
2016-02-11 15:31:26 +0100

    add namespace parameter in CachedRepository

--- app/repositories/cached_repository.rb
+++ app/repositories/cached_repository.rb
@@ -1,15 +1,16 @@
 class CachedRepository
-  def initialize(repository)
+  def initialize(repository, namespace)
+    @namespace = namespace
     @object = repository
   end
 
   def method_missing(name)
-    Rails.cache.fetch("#{object.class.name}.#{name}") do
+    Rails.cache.fetch("#{namespace}.#{name}") do
       object.public_send(name).to_a.uniq(&:id)
     end
   end
 
   private
 
-  attr_reader :object
+  attr_reader :object, :namespace
 end
