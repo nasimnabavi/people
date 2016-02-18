cdacf8a00e25eaa62ad50489df728b8d17b4c255
smnkrt <szymon.krywult@netguru.pl>
2016-02-18 11:10:09 +0100

    udpate specs

--- spec/features/project_potential_spec.rb
+++ spec/features/project_potential_spec.rb
@@ -21,10 +21,10 @@ describe 'Potential project', js: true do
       expect(page).to have_content("#{user.decorate.name}")
     end
 
-    it 'deletes membership when project is updated to nonpotential' do
+    it 'shows membership when project is potential' do
       click_button('Save')
       visit user_path(user)
-      expect(page).not_to have_content(project.name)
+      expect(page).to have_content(project.name)
     end
   end
 
